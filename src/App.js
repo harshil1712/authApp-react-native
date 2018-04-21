3ecec0c52d461fdad113384e1a0791db1d01b6b8
Harshil Agrawal <ghagrawal17@gmail.com>
2018-04-22 00:24:49 +0530

    firebase setup initiated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component  {
+    componentWillMount(){
+        firebase.initializeApp(
+            {
+                apiKey: "AIzaSyAt2OWZG3kKXDgZT1td9BFum9bl18jdjQo",
+                authDomain: "signin-page-78d90.firebaseapp.com",
+                databaseURL: "https://signin-page-78d90.firebaseio.com",
+                projectId: "signin-page-78d90",
+                storageBucket: "signin-page-78d90.appspot.com",
+                messagingSenderId: "758509980173"
+            }
+        )
+    }
     render () {
         return (
             <View>
