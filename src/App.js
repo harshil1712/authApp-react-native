b7cc3d52ce0f02c3b6be0eb0bd26f3538757b370
Harshil Agrawal <ghagrawal17@gmail.com>
2018-04-24 16:52:52 +0530

    Email input completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component  {
     componentWillMount(){
@@ -14,13 +15,14 @@ class App extends Component  {
                 storageBucket: "signin-page-78d90.appspot.com",
                 messagingSenderId: "758509980173"
             }
-        )
+        );
     }
+    
     render () {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>Hello</Text>
+                <LoginForm />
             </View>
         )
     }
